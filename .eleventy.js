f11373d008439ed74e231653b89e8a6bc9e4e451
xavier jouppe <xajouppe@gmail.com>
2023-05-20 13:27:57 +0200

    feat: fun with css

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,22 @@
+const sass = require("sass");
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addTemplateFormats("scss");
+
+  // Creates the extension for use
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+
+    compile: async function (inputContent) {
+      let result = sass.compileString(inputContent);
+
+      // This is the render function, `data` is the full data cascade
+      return async (data) => {
+        return result.css;
+      };
+    },
+  });
+
   eleventyConfig.addShortcode("year", function () {
     const d = new Date();
     return d.getFullYear();
@@ -6,6 +24,7 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/font");
+  eleventyConfig.addPassthroughCopy("src/images");
 
   return {
     dir: { input: "src", output: "_site" },
