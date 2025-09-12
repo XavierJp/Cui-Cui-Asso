81c19164a8e34d48c987394b655980dc2270f481
xavier jouppe <xajouppe@gmail.com>
2025-09-12 16:15:26 +0200

    feat: basic revision mechanism for cache

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
+const path = require("node:path");
 const sass = require("sass");
 const MarkdownIt = require("markdown-it");
+const crypto = require("crypto");
 
 // https://www.npmjs.com/package/markdown-it
 const md = new MarkdownIt({
@@ -7,21 +9,51 @@ const md = new MarkdownIt({
   linkify: true,
 });
 
+// Asset manifest to track hashed filenames
+const assetManifest = {};
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addTemplateFormats("scss");
 
   // Creates the extension for use
   eleventyConfig.addExtension("scss", {
     outputFileExtension: "css",
+    compile: async function (inputContent, inputPath) {
+      let parsed = path.parse(inputPath);
 
-    compile: async function (inputContent) {
-      let result = sass.compileString(inputContent);
+			let result = sass.compileString(inputContent, {
+				loadPaths: [
+					parsed.dir || ".",
+					this.config.dir.includes,
+				],
+				style: "compressed" // Minify the CSS
+			});
 
       // This is the render function, `data` is the full data cascade
       return async (data) => {
         return result.css;
       };
     },
+    compileOptions: {
+      permalink: function(_contents, inputPath) {
+        const parsed = path.parse(inputPath);
+
+        // compile option run before compile. We cannot hash it. Lets use a random id.
+        const random = crypto.randomUUID().slice(0,6);
+
+        const originalPath = `/css/${parsed.name}.css`;
+        const hashedPath = `/css/${parsed.name}.${random}.css`;
+
+        // Store in manifest
+        assetManifest[originalPath] = hashedPath;
+        return hashedPath;
+      }
+    }
+  });
+
+  // Add rev filter for asset hashing
+  eleventyConfig.addFilter("rev", function(url) {
+    return assetManifest[url] || url;
   });
 
   eleventyConfig.addShortcode("year", function () {
@@ -36,8 +68,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addShortcode("markdown", (str) => {
     return md.render(str);
   });
+  
 
-  eleventyConfig.addPassthroughCopy("src/css");
+
   eleventyConfig.addPassthroughCopy("src/font");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/js");
